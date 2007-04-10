1d97823cbb8f3c7930f0ca7e4ae0045daf67efd1
Craig Knudsen <craig@k5n.us>
2007-04-10 11:48:29 +0000

    Updated

--- src/us/k5n/journal/Main.java
+++ src/us/k5n/journal/Main.java
@@ -1,1 +1,21 @@
-// TODO
+package us.k5n.journal;
+
+import us.k5n.ical.*;
+
+/**
+ * Journal application main class.
+ * 
+ * @version $Id: Main.java,v 1.2 2007-04-10 11:48:29 cknudsen Exp $
+ * @author Craig Knudsen, craig@k5n.us
+ */
+public class Main {
+	
+	public Main ()
+	{
+	  // TODO
+	}
+
+	public static void main ( String args[] ) {
+		new Main ();
+	}
+}
